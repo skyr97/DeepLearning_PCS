d6e61c1c8355f34f53e1180ddc3eb3574fc03af7
skyr97 <dekudeku3a@gmail.com>
2021-02-16 00:23:36 +0800

    02_16例行提交，已恢复整体代码，暂不能运行

--- constellationshaping.py
+++ constellationshaping.py
@@ -166,6 +166,133 @@ class AddLossLayer(layers.Layer):
 class BaseConstellationShaping:
     def __init__(self, M):
         self.M = M
+        self.bits_per_sym=int(math.log2(self.M))
+    
+
+class SymbolwiseShaping(BaseConstellationShaping):
+    def __init__(self,M,snr,epochs,cce_weight=0.0,bce_weight=1.0,is_ebn0=False):
+        super(SymbolwiseShaping,self).__init__(M=M)
+        self.esn0_snr=snr
+        if is_ebn0:
+            self.esn0_snr=self.esn0_snr+10*math.log10(self.bits_per_sym)
+        self.noise_sigma = math.sqrt(0.5/math.pow(10,self.esn0_snr/10))
+        self.epochs=epochs
+        self.batch_size=1500
+        self.steps_per_epoch=1000
+        self.prob_layers=5
+        self.prob_nodes=120
+        self.mapper_layers=5
+        self.mapper_nodes=120
+        self.learning_rate=1e-4
+        self.decode_layers=5
+        self.decode_nodes=120
+        self.cce_weight=cce_weight
+        self.bce_weight=bce_weight
+        self.snr_inputs=keras.Input(shape=1,name="snr_inputs")
+        self.gumbel_inputs=keras.Input(shape=self.M,name="gumbel_inputs")
+        self.noise_inputs=keras.Input(shape=2,name="noise_inputs")
+
+    def _prob(self):
+        logits=layers.Dense(self.prob_nodes,activation='relu')(self.snr_inputs)
+        for _ in range(self.prob_layers-1):
+            logits=layers.Dense(self.prob_nodes,activation='relu')(logits)
+        self.logits=layers.Dense(self.M,activation='linear',name="logits")(logits)
+        self.prob=layers.Softmax(name="softmax_prob")(self.logits)
+    
+    def _symbols(self):
+        self.sym_sample = layers.Softmax()(self.gumbel_inputs + self.logits)
+        self.sym_dec = tf.argmax(self.sym_sample,axis=-1)
+        self.sym_onehot = GumbelSampler()(self.sym_sample)
+        self.binary_sym = BinaryOutGumbelSampler(name='binary_symbols')(self.sym_sample)
+        self.sym_concat = layers.Concatenate(axis=-1)([self.sym_onehot,self.binary_sym])
+    
+    def _cons_mapper(self):
+        in_phase=layers.Dense(self.mapper_nodes,activation='relu')(self.snr_inputs)
+        quadra_phase = layers.Dense(self.mapper_nodes,activation='relu')(self.snr_inputs)
+        for _ in range(self.mapper_layers-1):
+            in_phase=layers.Dense(self.mapper_nodes,activation='relu')(in_phase)
+            quadra_phase=layers.Dense(self.mapper_nodes,activation='relu')(quadra_phase)
+        in_phase=layers.Dense(self.M+self.bits_per_sym,activation='linear')(in_phase)
+        quadra_phase=layers.Dense(self.M+self.bits_per_sym,activation='linear')(quadra_phase)
+        self.in_phase=tf.reduce_mean(in_phase,axis=0)
+        self.quadra_phase=tf.reduce_mean(quadra_phase,axis=0)
+        self.in_phase=tf.reshape(self.in_phase,shape=[-1,1])
+        self.quadra_phase=tf.reshape(self.quadra_phase,shape=[-1,1])
+        self.mapper_unnorm=layers.Concatenate(axis=-1)([self.in_phase,self.quadra_phase])
+        base_sym_onehot = keras.utils.to_categorical(np.arange(self.M),num_classes=self.M)
+        base_sym_bin = np.matmul(base_sym_onehot,mapper_dict[self.M])
+        base_sym_concat = np.concatenate((base_sym_onehot,base_sym_bin),axis=-1)
+        base_cons = tf.matmul(base_sym_concat,self.mapper_unnorm) # shape: (M,2)
+        energy = tf.reshape(tf.reduce_sum(tf.math.square(base_cons),axis=-1),shape=[-1,1])
+        energy = tf.reduce_mean(tf.matmul(self.prob,energy))
+        self.mapper_norm = tf.divide(self.mapper_unnorm,tf.math.sqrt(energy))
+        self.tx_norm = tf.matmul(self.sym_concat,self.mapper_norm) # shape:(batch_size,2)
+    
+    def _channel(self):
+        self.rx = layers.Add(name="channel")([self.tx_norm,self.noise_inputs])
+    
+    def _decode_softmax(self):
+        d_temp = layers.Dense(self.decode_nodes,activation='relu')(self.rx)
+        for _ in range(self.decode_layers-1):
+            d_temp=layers.Dense(self.decode_nodes,activation='relu')(d_temp)
+        self.decode_out_softmax=layers.Dense(self.M,activation='softmax')(d_temp)
+        self.decode_hard_sym = tf.argmax(self.decode_out_softmax,axis=-1)
+        self.ser = keras.backend.mean(self.decode_hard_sym!=self.sym_dec)
+        sym2bin = tf.convert_to_tensor(mapper_dict[self.M])
+        hard_bits = tf.matmul(self.decode_hard_sym,sym2bin)
+        self.ber_from_sym = keras.backend.mean(hard_bits!=self.binary_sym)        
+
+    def _decode_sigmoid(self):
+        d_temp = layers.Dense(self.decode_nodes,activation='relu')(self.rx)
+        for _ in range(self.decode_layers-1):
+            d_temp=layers.Dense(self.decode_nodes,activation='relu')(d_temp)
+        self.decode_out_sigmoid = layers.Dense(self.bits_per_sym,activation='sigmoid')(d_temp)
+        self.decode_hard_bits = tf.math.rint(self.decode_out_sigmoid)
+        self.ber = keras.backend.mean(self.decode_hard_bits!=self.binary_sym)
+        self.ser_from_bits = tf.reduce_mean(tf.math.reduce_prod(tf.cast(self.decode_hard_bits!=self.binary_sym,tf.float32),axis=-1))
+    
+    def create_model(self):
+        self._prob()
+        self._symbols()
+        self._cons_mapper()
+        self._channel()
+        self._decode_softmax()
+        self._decode_sigmoid()
+        self.model_whole = keras.Model(inputs=[self.snr_inputs,self.gumbel_inputs,self.noise_inputs],outputs=[self.decode_out_softmax,self.decode_out_sigmoid])
+        self.model_whole:keras.Model
+        cce=keras.losses.categorical_crossentropy(y_true=self.sym_onehot,y_pred=self.decode_out_softmax)
+        bce=keras.losses.binary_crossentropy(y_true=self.binary_sym,y_pred=self.decode_out_sigmoid)
+        entropy_log = keras.losses.categorical_crossentropy(self.prob,self.prob)
+        mutual_info_bit = tf.divide(cce-entropy_log,log_2)
+        mix_loss = self.cce_weight*cce+self.bce_weight*bce-entropy_log
+        self.model_whole.add_loss(mix_loss)
+        self.model_whole.add_metric(mutual_info_bit,name="mutual_info")
+        self.model_whole.add_metric(self.ser,name="ser")
+        self.model_whole.add_metric(self.ber_from_sym,name="ber_from_sym")
+        self.model_whole.add_metric(self.ber,name="ber")
+        self.model_whole.add_metric(self.ser_from_bits,name="ser_from_bits")
+        self.model_whole.compile(optimizer=keras.optimizers.Adam(self.learning_rate))
+    
+    def infinity_data_generator(self):
+        snr_data = tf.ones(shape=[self.batch_size,1],dtype='float32')
+        while True:
+            gumbel_data = standard_gumbel(shape=[self.batch_size,self.M])
+            noise = tf.random.normal(shape=[self.batch_size,2],stddev=self.noise_sigma)
+            yield [snr_data,gumbel_data,noise]
+    
+    def train(self):
+        callbacks = [
+            keras.callbacks.ModelCheckpoint(filepath="./model/test1/",monitor="mutual_info",verbose=1,save_best_only=True,mode='max'),
+            keras.callbacks.EarlyStopping(monitor="mutual_info",patience=2,verbose=1,mode='max')
+        ]
+        self.model_whole.fit(self.infinity_data_generator,epochs=self.epochs,steps_per_epoch=self.steps_per_epoch,callbacks=callbacks,verbose=2)
+    
+    def evaluate(self):
+        pass
+    
+    def cons_prob_plot(self):
+        pass
+    
 
 
 class BitwiseShaping(BaseConstellationShaping):
@@ -285,15 +412,19 @@ class BitwiseShaping(BaseConstellationShaping):
 
 
 if __name__ == '__main__':
-    comm.set_device(only_cpu=True)
+    comm.set_device(gpu_id=1,gpu_mem=4096)
     comm.redirect2log('log.log')
-    bitwise_shaper = BitwiseShaping(M=64, snr=14, EborEs=True)
-    bitwise_shaper.create_model()
-    bitwise_shaper.model.summary()
-    # keras.utils.plot_model(bitwise_shaper.model,show_shapes=True)
-    # bitwise_shaper.model.evaluate(bitwise_shaper.train_data_generator(), steps=10)
-    bitwise_shaper.train()
-    bitwise_shaper.cons_point_generate()
+    print("tf version:",tf.__version__)
+    symwise_shaper = SymbolwiseShaping(M=16,snr=8,epochs=10,cce_weight=0.5,bce_weight=1.0,is_ebn0=False)
+    symwise_shaper.create_model()
+    symwise_shaper.train()
+    # bitwise_shaper = BitwiseShaping(M=64, snr=14, EborEs=True)
+    # bitwise_shaper.create_model()
+    # bitwise_shaper.model.summary()
+    # # keras.utils.plot_model(bitwise_shaper.model,show_shapes=True)
+    # # bitwise_shaper.model.evaluate(bitwise_shaper.train_data_generator(), steps=10)
+    # bitwise_shaper.train()
+    # bitwise_shaper.cons_point_generate()
 
     # debug
     # 前向可以跑通
